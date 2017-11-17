522c634f9ab3e229e50c1da2db50866ac1609a1b
lletfrix <lletfrix@gmail.com>
2017-11-17 18:37:13 +0100

    3p Initial commit

---
+++ 3practica/ordenacion.c
@@ -0,0 +1,221 @@
+/**
+ *
+ * Descripcion: Implementacion de funciones de ordenacion
+ *
+ * Fichero: ordenacion.c
+ * Autor: Carlos Aguirre
+ * Version: 1.0
+ * Fecha: 16-09-2017
+ *
+ */
+
+
+#include "ordenacion.h"
+#include <math.h>
+#include <stdlib.h>
+/* Funciones privada */
+int _merge_ (int* tabla, int ip, int iu, int imedio);
+int _partir_(int* tabla, int ip, int iu, int *pos);
+int _medio_ (int *tabla, int ip, int iu, int *pos);
+int _medio_avg_ (int *tabla, int ip, int iu, int *pos);
+int _medio_stat_ (int *tabla, int ip, int iu, int *pos);
+
+/***************************************************/
+/* Funcion: BubbleSort    Fecha: 19/10/2017        */
+/* Autores: Rafael Sánchez, Sergio Galán           */
+/*                                                 */
+/* Funcion que ordena una tabla por el método de   */
+/* la burbuja                                      */
+/*                                                 */
+/* Entrada:                                        */
+/* int* tabla: Array de enteros a ordenar          */
+/* int ip: Indice del elemento desde el que ordenar*/
+/* int iu: Indice del elemento hasta el que ordenar*/
+/* Salida:                                         */
+/* int: Numero de veces que se ejecutó la OB       */
+/* ERR en caso de error                            */
+/***************************************************/
+int BubbleSort(int* tabla, int ip, int iu)
+{
+  int i,j,temp, ret=0;
+  for ( i = iu; i >= ip+1; i--){
+    for (j = ip; j <= i-1; j++){
+      ret++;
+      if (tabla[j] > tabla[j+1]){
+        temp = tabla[j];
+        tabla[j] = tabla[j+1];
+        tabla[j+1]=temp;
+      }
+    }
+  }
+  if(ret==0){
+    return ERR;
+  }
+  return ret;
+}
+
+
+/***************************************************/
+/* Funcion: MergeSort    Fecha: 20/10/2017         */
+/* Autores: Rafael Sánchez, Sergio Galán           */
+/*                                                 */
+/* Funcion que ordena una tabla por el método de   */
+/* merge sort                                      */
+/*                                                 */
+/* Entrada:                                        */
+/* int* tabla: Array de enteros a ordenar          */
+/* int ip: Indice del elemento desde el que ordenar*/
+/* int iu: Indice del elemento hasta el que ordenar*/
+/* Salida:                                         */
+/* int: Numero de veces que se ejecutó la OB       */
+/* ERR en caso de error                            */
+/***************************************************/
+int MergeSort (int* tabla, int ip, int  iu){
+  int im, errcode1=0, errcode2=0;
+  if (!tabla || ip > iu){
+    return ERR;
+  }
+  if (ip == iu){
+    return 0;
+  }
+  im = floor((ip+iu)/2);
+  errcode1 = MergeSort(tabla, ip, im);
+    if(errcode1 == ERR){
+      return ERR;
+    }
+  errcode2 = MergeSort(tabla, im+1, iu);
+    if(errcode2 == ERR){
+      return ERR;
+    }
+  return _merge_(tabla, ip, iu, im) + errcode1 + errcode2;
+}
+
+/***************************************************/
+/* Funcion: QuickSort    Fecha: 20/10/2017         */
+/* Autores: Rafael Sánchez, Sergio Galán           */
+/*                                                 */
+/* Funcion que ordena una tabla por el método de   */
+/* Quick Sort                                     */
+/*                                                 */
+/* Entrada:                                        */
+/* int* tabla: Array de enteros a ordenar          */
+/* int ip: Indice del elemento desde el que ordenar*/
+/* int iu: Indice del elemento hasta el que ordenar*/
+/* Salida:                                         */
+/* int: Numero de veces que se ejecutó la OB       */
+/* ERR en caso de error                            */
+/***************************************************/
+int QuickSort (int* tabla, int ip, int  iu){
+  int errcode1 = 0, errcode2 = 0, c, pivote;
+
+  if (!tabla || ip > iu){
+    return ERR;
+  }
+  if (ip == iu){
+    return 0;
+  }
+  c = _partir_(tabla, ip, iu, &pivote);
+  if(ip < pivote -1){
+    errcode1 = QuickSort(tabla, ip, pivote-1);
+    if(errcode1 == ERR){
+      return ERR;
+    }
+  }
+  if(pivote + 1 < iu){
+    errcode2 = QuickSort(tabla, pivote + 1, iu);
+    if(errcode2 == ERR){
+      return ERR;
+    }
+  }
+  return errcode1 + errcode2 + c;
+}
+
+int _merge_ (int* tabla, int ip, int iu,  int imedio){
+  int* taux, i,j,k, nOb=0;
+  if(!tabla){
+    return ERR;
+  }
+  taux = calloc(iu-ip+1, sizeof(int));
+  if(!taux){
+    return ERR;
+  }
+  i=ip;
+  k=0;
+  j=imedio+1;
+  while (i <= imedio && j<=iu) {
+    if(tabla[i] < tabla[j]){
+      taux[k]=tabla[i];
+      i++;
+    }else{
+      taux[k]=tabla[j];
+      j++;
+    }
+    k++;nOb++;
+  }
+  if (i>imedio){
+    while (j <= iu) {
+      taux[k]=tabla[j];
+      j++;k++;
+    }
+  }else if (j>iu){
+    while (i <= imedio){
+      taux[k]=tabla[i];
+      i++;k++;
+    }
+  }
+  for (k=0; ip <=iu; ip++, k++) {
+    tabla[ip]=taux[k];
+  }
+  free(taux);
+  return nOb;
+}
+
+
+int _partir_(int* tabla, int ip, int iu, int *pos){
+  int temp, k, i, nOb = 0, ret;
+  ret = _medio_stat_(tabla, ip, iu, pos);
+  if(ERR == ret){
+    return ERR;
+  }
+  nOb += ret;
+  k = tabla[*pos];
+  temp = tabla[*pos];
+  tabla[*pos] = tabla[ip];
+  tabla[ip] = temp;
+  *pos = ip;
+  for (i = ip+1; i <= iu; i++) {
+    if(tabla[i] < k){
+      *pos = (*pos)+1 ;
+      temp = tabla[i];
+      tabla[i] = tabla[*pos];
+      tabla[*pos] = temp;
+    }
+    nOb++;
+  }
+  temp = tabla[ip];
+  tabla[ip] = tabla[*pos];
+  tabla[*pos] = temp;
+  return nOb;
+}
+int _medio_ (int *tabla, int ip, int iu, int *pos){
+  *pos = ip;
+  return 0;
+}
+int _medio_avg_ (int *tabla, int ip, int iu, int *pos){
+  *pos = floor((ip+iu)/2);
+  return 0;
+}
+int _medio_stat_ (int *tabla, int ip, int iu, int *pos){
+  int medio = floor((ip+iu)/2);
+  int a = tabla[ip], b = tabla[medio], c = tabla[iu];
+  if ((b - a) * (c - b) >= 0){ /* c > b > a || c < b < a */
+    *pos = medio;
+    return 1;
+  }
+  if((a - b) * (c - a) >= 0){
+    *pos = ip;
+    return 2;
+  }
+  *pos = iu;
+  return 2;
+}
---
+++ 3practica/tiempos.c
@@ -0,0 +1,184 @@
+/**
+ *
+ * Descripcion: Implementacion de funciones de tiempo
+ *
+ * Fichero: tiempos.c
+ * Autor: Carlos Aguirre Maeso
+ * Version: 1.0
+ * Fecha: 16-09-2017
+ *
+ */
+
+#include "tiempos.h"
+#include "ordenacion.h"
+#include "permutaciones.h"
+#include <math.h>
+
+/******************************************************/
+/* Funcion: tiempo_medio_ordenacion Fecha: 19/10/2017 */
+/* Autores: Rafael Sánchez, Sergio Galán              */
+/*                                                    */
+/* Funcion que guarda en un tipo de dato TIEMPO, la   */
+/* informacion relacionada con un metodo de ordenacion*/
+/*                                                    */
+/* Entrada:                                           */
+/* pfunc_ordena metodo: Puntero a una funcion de      */
+/* ordenacion                                         */
+/* int n_perms: Numero de permutaciones               */
+/* int N: Numero de elementos de cada permutacion     */
+/* PTIEMPO ptiempo: Puntero al struct TIEMPO donde se */
+/* guardaran los datos                                */
+/* Salida:                                            */
+/* short: OK si no surgió ningún fallo                */
+/* ERR en caso de error                               */
+/******************************************************/
+short tiempo_medio_ordenacion(pfunc_ordena metodo,
+                              int n_perms,
+                              int N,
+                              PTIEMPO ptiempo)
+{
+  int **permutaciones, current_ob, min_ob, max_ob, all_ob=0, i;
+  clock_t t_start, t_end, t_total=0;
+  double t_avg, avg_ob, clocks_per_sec_d;
+
+  if(!ptiempo || N < 0 || n_perms < 0){
+    return ERR;
+  }
+
+  ptiempo->n_elems = n_perms;
+  ptiempo->N = N;
+  permutaciones = genera_permutaciones(n_perms, N);
+  if(!permutaciones){
+    return ERR;
+  }
+  for (i = 0; i < n_perms; i++){
+    t_start = clock();
+    current_ob = metodo(permutaciones[i], 0, N-1);
+    t_end = clock();
+    t_total += t_end - t_start;
+    if (!i){
+      min_ob = current_ob;
+      max_ob = current_ob;
+    }else{
+      if (current_ob < min_ob) min_ob = current_ob;
+      if (current_ob > max_ob) max_ob = current_ob;
+    }
+    all_ob += current_ob;
+  }
+  clocks_per_sec_d = (double) CLOCKS_PER_SEC;
+  avg_ob = (double) all_ob/n_perms;
+  t_avg = (double) t_total / n_perms;
+  ptiempo->tiempo = t_avg *= 1000.0f/(clocks_per_sec_d);
+  ptiempo->min_ob = min_ob;
+  ptiempo->medio_ob = avg_ob;
+  ptiempo->max_ob = max_ob;
+
+  for(i = 0; i<n_perms; i++){
+    free(permutaciones[i]);
+  }
+  free(permutaciones);
+
+  return OK;
+}
+
+/***********************************************************/
+/* Funcion: genera_tiempos_ordenacion Fecha: 19/10/2017    */
+/* Autores: Rafael Sánchez, Sergio Galán                   */
+/*                                                         */
+/* Funcion que genera un array de TIEMPO haciendo          */
+/* llamadas sucesivas a la funcion tiempo_medio_ordenacion */
+/* con un tamaño de permutacion distinto cada vez. Luego,  */
+/* tras sucesivas llamadas a guarda_tabla_tiempos, genera  */
+/* un fichero con la informacion del array de TIEMPO.      */
+/*                                                         */
+/* Entrada:                                                */
+/* pfunc_ordena metodo: Puntero a una funcion de           */
+/* ordenacion                                              */
+/* char* fichero: nombre del fichero al que escribir       */
+/* int num_min: tamaño minimo de la permutacion            */
+/* int num_max: tamaño maximo de la permutacion            */
+/* int incr: Variación del tamaño de permutacion en cada   */
+/* llamada a tiempo_medio_ordenacion                       */
+/* int n_perms: Numero de permutaciones                    */
+/* Salida:                                                 */
+/* short: OK si no surgió ningún fallo                     */
+/* ERR en caso de error                                    */
+/***********************************************************/
+short genera_tiempos_ordenacion(pfunc_ordena metodo, char* fichero,
+                                int num_min, int num_max,
+                                int incr, int n_perms)
+{
+  PTIEMPO ptiempo;
+  int i, size, j;
+
+  if (!metodo || !fichero || incr <= 0 || num_min < 0 || num_max < 0 || num_min > num_max || n_perms <= 0){
+    return ERR;
+  }
+
+  size = ceil((num_max - num_min)/incr);
+  ptiempo = calloc(size+1, sizeof(TIEMPO));
+  if(!ptiempo){
+    return ERR;
+  }
+  for(i=num_min, j=0; i<=num_max; i+=incr, j++){
+    if(ERR == tiempo_medio_ordenacion(metodo, n_perms, i, &ptiempo[j])){
+      free(ptiempo);
+      return ERR;
+    }
+  }
+  for(j=0;j<=size;j++){
+    if (ERR == guarda_tabla_tiempos(fichero, &ptiempo[j], j)){
+      free(ptiempo);
+      return ERR;
+    }
+  }
+  free(ptiempo);
+  return OK;
+}
+
+/***********************************************************/
+/* Funcion: genera_tiempos_ordenacion Fecha: 19/10/2017    */
+/* Autores: Rafael Sánchez, Sergio Galán                   */
+/*                                                         */
+/* Funcion que abre un fichero y escribe en el la          */
+/* informacion de un array de TIEMPO                       */
+/*                                                         */
+/* Entrada:                                                */
+/* char* fichero: nombre del fichero al que escribir       */
+/* int num_min: tamaño minimo de la permutacion            */
+/* PTIEMPO tiempo:Array que guarda los tiempos de ejecucion*/
+/* int n_tiempos: Numero de elementos del array TIEMPO     */
+/* Salida:                                                 */
+/* short: OK si no surgió ningún fallo                     */
+/* ERR en caso de error                                    */
+/***********************************************************/
+short guarda_tabla_tiempos(char* fichero, PTIEMPO tiempo, int n_tiempos)
+{
+  FILE* fp;
+  fp = fopen(fichero, "a");
+  if(!fp){
+    return ERR;
+  }
+  fprintf(fp, "%d %f %f %d %d\n",tiempo->N, tiempo->tiempo, tiempo->medio_ob, tiempo->min_ob, tiempo->max_ob);
+  fclose(fp);
+  return OK;
+
+}
+
+
+short genera_tiempos_busqueda(pfunc_busqueda metodo, pfunc_generador_claves generador,
+                                int orden, char* fichero,
+                                int num_min, int num_max,
+                                int incr, int n_veces)
+{
+
+}
+
+short tiempo_medio_busqueda(pfunc_busqueda metodo, pfunc_generador_claves generador,
+                              int orden,
+                              int N,
+                              int n_veces,
+                              PTIEMPO ptiempo)
+{
+
+}
